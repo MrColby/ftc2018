262310a99a8ffcc8d6369dd55abeb5ad32e8cb45
witjac24 <jack.witt06@gmail.com>
2018-10-03 18:15:59 -0500

    Jack from computer 1

--- TeamCode/src/main/java/org/isd300/ftc/Test.java
+++ TeamCode/src/main/java/org/isd300/ftc/Test.java
@@ -3,5 +3,6 @@ package org.isd300.ftc;
 // test yet again!!!!!!
 // Dana was here. Computers 1, 2 and 3.
 // Kami was here too!!!!! and from computers 2 and 3.
+// Jack was here from computer 1
 public class Test {
 }
