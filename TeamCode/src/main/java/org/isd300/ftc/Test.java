07eedf87492502dbb99082e07ae7bef8306b0a58
Izaak <izaakburros2004>
2018-10-10 14:21:47 -0500

    Izaakisthebest

--- TeamCode/src/main/java/org/isd300/ftc/Test.java
+++ TeamCode/src/main/java/org/isd300/ftc/Test.java
@@ -1,8 +1,10 @@
 package org.isd300.ftc;
 
+// test yet again!!!!!
 // test yet again!!!!!!
 // Dana was here. Computers 1, 2 and 3.
 // Kami was here too!!!!! and from computers 2 and 3.
 // Jack was here from computer 1
+
 public class Test {
 }
--- TeamCode/src/main/java/org/isd300/ftc/ind/izaak/IzaakTheBestCoder.java
+++ TeamCode/src/main/java/org/isd300/ftc/ind/izaak/IzaakTheBestCoder.java
@@ -1,4 +1,34 @@
 package org.isd300.ftc.ind.izaak;
 
-public class IzaakTheBestCoder {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.isd300.ftc.tilerunner.TileRunnerBot;
+
+@Autonomous(name="YeetBot: Auto Drive By Time", group="Pushbot")
+
+public class IzaakTheBestCoder extends LinearOpMode {
+
+    private TileRunnerBot totBot;
+
+    private ElapsedTime timer = new ElapsedTime();
+
+    public void runOpMode() {
+
+        totBot = new TileRunnerBot(this.hardwareMap,this.telemetry);
+
+        this.waitForStart();
+
+        totBot.drive(69, 69, 96, 25);
+        timer.reset();
+        while(timer.milliseconds()<3000 && this.opModeIsActive()) {
+        }
+
+
+
+        totBot.drive(0, 0, 0, 0);
+
+
+    }
 }
