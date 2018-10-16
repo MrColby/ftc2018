4c8e443a2984fa412a8941e4e975679debf28ebb
fridan24 <dana.elizabeth.friesen@gmail.com>
2018-10-16 16:16:43 -0500

    DanaBot01!

--- TeamCode/src/main/java/org/isd300/ftc/ind/dana/DanaBot.java
+++ TeamCode/src/main/java/org/isd300/ftc/ind/dana/DanaBot.java
@@ -14,15 +14,25 @@ public class DanaBot extends LinearOpMode {
     private ElapsedTime timer = new ElapsedTime();
 
     public void runOpMode() {
-
+        totBot = new TileRunnerBot(this.hardwareMap,this.telemetry);
         this.waitForStart();
 
         timer.reset();
+        //fl, fr, rl, rr
+        totBot.drive(1, 1, 1, 1);
+        while (timer.milliseconds() < 1000 && this.opModeIsActive()) {
+        }
 
-        totBot.drive(100, 100, 0, 0);
+        timer.reset();
+        //fl, fr, rl, rr
+        totBot.drive(0, 0,0 , 0);
+        while (timer.milliseconds() < 2000 && this.opModeIsActive()) {
+        }
 
-        while (timer.milliseconds() < 3000 && this.opModeIsActive()) {
-
+        timer.reset();
+        //fl, fr, rl, rr
+        totBot.drive(-.2, 1,-.2 , 1);
+        while (timer.milliseconds() < 2000 && this.opModeIsActive()) {
         }
 
 
