5ea3667c445f8cce11ed8b6808ebf07572f40741
Izaak <izaakburros2004>
2018-10-22 17:05:52 -0500

    Izaakisthebest

--- TeamCode/src/main/java/org/isd300/ftc/ind/kami/KamiBot.java
+++ TeamCode/src/main/java/org/isd300/ftc/ind/kami/KamiBot.java
@@ -19,8 +19,11 @@ public class KamiBot extends LinearOpMode {
         totBot = new TileRunnerBot(this.hardwareMap,this.telemetry);
 
         this.waitForStart();
-        this.totBot.getEyestalkColor();
-        totBot.drive(10, 10, 10, 20);
+        while (this.opModeIsActive()) {
+            this.totBot.getEyestalkColor();
+        }
+
+        /*totBot.drive(10, 10, 10, 20);
         timer.reset();
         while(timer.milliseconds()<1700 && this.opModeIsActive()) {
         }
@@ -41,8 +44,8 @@ public class KamiBot extends LinearOpMode {
         totBot.drive(10, 20, 10, 20);
         timer.reset();
         while(timer.milliseconds()<2000 && this.opModeIsActive()) {
-        }
-        totBot.drive(0, 0, 0, 0);
+        }*/
+        //totBot.drive(0, 0, 0, 0);
 
 
     }
