7fa9bc487e7ceee311afdee37e892a162be9434c
WALMAX23 <maxwalz304@gmail.com>
2018-10-30 16:25:49 -0500

    MaxBot

--- TeamCode/src/main/java/org/isd300/ftc/ind/max/MaxBot.java
+++ TeamCode/src/main/java/org/isd300/ftc/ind/max/MaxBot.java
@@ -16,29 +16,110 @@ public class MaxBot extends LinearOpMode {
 
     public void runOpMode() {
 
-        totBot = new TileRunnerBot(this.hardwareMap,this.telemetry);
+        boolean foundDistance = false;
 
+        totBot = new TileRunnerBot(this.hardwareMap, this.telemetry);
+        //wait for start button
         this.waitForStart();
 
-        //fl, fr, rl, rr
-        totBot.drive(1, 1, 1, 1);
+        while (this.opModeIsActive() ) {
+
+            totBot.drive(0.125, 0.125, 0.125, 0.125);
+
+            //drive for a 1/4 of a second
+            timer.reset();
+            while (timer.milliseconds() < 250) {
+
+            }
+
+            double distance = this.totBot.getDistance();
+            String msg = "Distance=" + distance;
+            this.totBot.message("", msg);
+
+            if (distance > 2) {
+                foundDistance = true;
+                totBot.drive(0, 0, 0, 0);
+            }
+
+            int red = this.totBot.getRed();
+            int blue = this.totBot.getBlue();
+            int green = this.totBot.getGreen();
+
+            msg = "Red=" + red + "Blue=" + blue + "Green=" + green;
+            this.totBot.message("", msg);
+
+
+        }
+    }
+
+    public void runOpModeColor() {
+
+        totBot = new TileRunnerBot(this.hardwareMap,this.telemetry);
+        //wait for start button
+        this.waitForStart();
+
+        //there is no yellow
+        boolean foundYellow = false;
+
+        //run until yellow is found
+        while (this.opModeIsActive() && foundYellow == false) {
+
+            totBot.drive(0.125,0.125,0.125,0.125);
+
+            //drive for a 1/4 of a second
+            timer.reset();
+            while (timer.milliseconds() < 250){
+            }
+
+            totBot.drive(0, 0, 0, 0);
+
+            int red = this.totBot.getRed();
+            int blue = this.totBot.getBlue();
+            int green = this.totBot.getGreen();
+
+            //drive for a 1/4 of a second
+            timer.reset();
+            while (timer.milliseconds() < 250){
+            }
+
+            //String msg = "Red=" + red + "Blue=" + blue + "Green=" + green;
+            //this.totBot.message("", msg);
+
+            if (red > 70 && green > 35) {
+                foundYellow = true;
+            }
+
+            double distance = this.totBot.getDistance();
+            String msg = "Distance=" + distance;
+            this.totBot.message("", msg);
+
+
+        }
+
+
+        /*totBot.drive(10, 10, 10, 20);
+        timer.reset();
+        while(timer.milliseconds()<1700 && this.opModeIsActive()) {
+        }
+// Happy Doughnut
+        totBot.drive(10, -10, 10, -10);
+        timer.reset();
+        while(timer.milliseconds()<1400 && this.opModeIsActive()) {
+        }
+
+        totBot.drive(10, 10, 10, 10);
+        timer.reset();
+        while(timer.milliseconds()<2550 && this.opModeIsActive()) {
+        }
+        totBot.drive(15, -15, 15, -15);
         timer.reset();
         while(timer.milliseconds()<500 && this.opModeIsActive()) {
         }
-
+        totBot.drive(10, 20, 10, 20);
         timer.reset();
-        while(timer.milliseconds()<1000 && this.opModeIsActive()) {
-        }
-
-        //fl, fr, rl, rr
-        totBot.drive(-1, -1, -1, -1);
-        timer.reset();
-        while(timer.milliseconds()<500 && this.opModeIsActive()) {
-        }
-
-
-
-        totBot.drive(0, 0, 0, 0);
+        while(timer.milliseconds()<2000 && this.opModeIsActive()) {
+        }*/
+        //totBot.drive(0, 0, 0, 0);
 
 
     }
