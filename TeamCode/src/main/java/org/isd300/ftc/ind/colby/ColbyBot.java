7f983ed754a29830d2182368cd38ef4e1505680f
MrColby <logan.colby@gmail.com>
2018-11-12 14:55:15 -0600

    BaseTileRunner

--- TeamCode/src/main/java/org/isd300/ftc/ind/colby/ColbyBot.java
+++ TeamCode/src/main/java/org/isd300/ftc/ind/colby/ColbyBot.java
@@ -4,15 +4,25 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.isd300.ftc.tilerunner.TileRunnerBot;
 
-@Autonomous(name="KamiBot", group="Kami")
+@Autonomous(name="ColbyBot", group="Colby")
 
 public class ColbyBot extends LinearOpMode {
 
+    // this is the robot. Make sure you create a new instance, or you'll learn the lesson of the NullPointerException
     private TileRunnerBot totBot;
 
+    // any time you need to perform some action for an amount of time, use this class-scoped timer.
     private ElapsedTime timer = new ElapsedTime();
+
+    // declare these variables at the class scope. That means that every method can read them.
+    // make them private so that only this class can interact with them.
+    private double distance = Double.NaN;
+    private int red = 0;
+    private int green = 0;
+    private int blue = 0;
 
     public void runOpMode() {
 
@@ -22,35 +32,71 @@ public class ColbyBot extends LinearOpMode {
         // wait for a user to push the start button
         this.waitForStart();
 
-        // start driving
 
-        // drive to target #1
 
-        // if 1 is right, celebrate and stop
+        // if 1 is right, celebrate and knock over the target. Drive to base.
         // else drive to target #2
-        // if 2 is right, celebrate and stop
-        // else drive to target #3
+        // if 2 is right, celebrate and knock over the target. Drive to base.
+        // else drive to target #3. If 3 is right, celebrate and knock over the target.
+        // drive to base
 
         boolean yellow = checkOutTargetOne();
         if (yellow) {
-            // do a happy dance
+            // knock over the target. Drive to base.
         }
         else {
             yellow = checkOutTargetTwo();
             /*if (yellow) {
-                // do a happy dance
+                // knock over the target. Drive to base.
             }
             else {
                 yellow = checkOutTargetThree();
+                if (hellow) {
+                // knock over the target
+                }
+                // drive to base
             }*/
         }
+
+        // make sure we're stopped
+        totBot.drive(0.0, 0.0, 0.0, 0.0);
 
     }
 
     private boolean checkOutTargetOne() {
 
-        // check distance
-        // if close, stop motors, check color
+        // start driving.
+        // frontLeft, frontRight, rearLeft, rearRight
+        totBot.drive(0.25, 0.25, 0.25, 0.25);
+
+        // drive to target #1. Add turns, etc.
+        // This is not sensor driving.
+        // reset the timer, and then do nothing for 3 seconds
+        this.timer.reset();
+
+        // notice that we can escape this loop EITHER by the timer, or if someone pushes the stop button.
+        // every loop must include the opModeIsActive call
+        // add turns, etc, so that we are lined up with the target.
+        while (this.opModeIsActive() && this.timer.milliseconds() < 3000) {
+
+        }
+
+        // this gets a current reading of distance and three primary colors
+        readColorSensor(DistanceUnit.CM);
+
+        // drive until we're close enough to get a measurement
+        while (this.opModeIsActive() && Double.isNaN(distance)) {
+            readColorSensor(DistanceUnit.CM);
+        }
+
+        // now drive until we're at the distance that we want
+        while (this.opModeIsActive() && distance > 10) {
+            readColorSensor(DistanceUnit.CM);
+        }
+
+        // we're there. Switch off the motors.
+        totBot.drive(0.0, 0.0, 0.0, 0.0);
+
         // if wrong color, return false
         // if right color, return true
         return false;
@@ -58,12 +104,10 @@ public class ColbyBot extends LinearOpMode {
 
     }
 
+
     private boolean checkOutTargetTwo() {
 
-        // check distance
-        // if close, stop motors, check color
-        // if wrong color, return false
-        // if right color, return true
+
         return false;
 
 
@@ -71,13 +115,22 @@ public class ColbyBot extends LinearOpMode {
 
     private boolean checkOutTargetThree() {
 
-        // check distance
-        // if close, stop motors, check color
-        // if wrong color, return false
-        // if right color, return true
+
         return false;
 
 
     }
+
+    private void readColorSensor(DistanceUnit distanceUnit) {
+        distance = this.totBot.getDistance(distanceUnit);
+        red = this.totBot.getRed();
+        green = this.totBot.getGreen();
+        blue = this.totBot.getBlue();
+        String msg = "Red: " + red + " Green: " + green + " Blue: " + blue + " Distance: " + distance;
+        this.totBot.message("", msg);
+    }
+
+
+
 
 }
