84a93caddf335d459d06666597c666e41e1104ae
MueCor24 <coreymuenzenberger@gmail.com>
2018-12-03 16:07:49 -0600

    Coreybot

---
+++ TeamCode/src/main/java/org/isd300/ftc/ind/corey/Coreybot.java
@@ -0,0 +1,73 @@
+package org.isd300.ftc.ind.corey;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.isd300.ftc.tilerunner.TileRunnerBot;
+
+@Autonomous(name="Coreybot", group="Corey")
+public class Coreybot extends LinearOpMode {
+
+    // this is the robot. Make sure you create a new instance, or you'll learn the lesson of the NullPointerException
+    private TileRunnerBot totBot;
+
+    // any time you need to perform some action for an amount of time, use this class-scoped timer.
+    private ElapsedTime timer = new ElapsedTime();
+
+    public void runOpMode() {
+
+        // create a robot instance
+        totBot = new TileRunnerBot(this.hardwareMap, this.telemetry);
+
+        // wait for a user to push the start button
+        this.waitForStart();
+
+        //this.run1();
+
+        //this.pause(1000);
+
+        this.run2();
+
+
+
+
+
+
+
+    }
+
+    private void pause(int millis) {
+        this.timer.reset();
+        while (this.timer.milliseconds() < millis) {
+            // do nothing
+        }
+
+    }
+
+    private void run1() {
+        // drive straight
+        totBot.drive(0.5, 0.5, 0.5, 0.5);
+
+        this.timer.reset();
+        while (this.timer.milliseconds() < 1000) {
+            // do nothing
+        }
+
+        totBot.drive(0.0, 0.0, 0.0, 0.0);
+    }
+
+    public void run2() {
+        //
+        totBot.drive(-0.5, 0.5, -0.5, 0.5);
+
+        this.timer.reset();
+        while (this.timer.milliseconds() < 1000) {
+            // do nothing
+        }
+
+        totBot.drive(0.0, 0.0, 0.0, 0.0);
+    }
+
+}
